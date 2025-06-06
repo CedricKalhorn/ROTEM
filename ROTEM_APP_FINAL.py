9b1d6a1a66000f9b3dec2efdaebc49ab60b22bf6
CedricKalhorn <c.a.f.kalhorn@student.tudelft.nl>
2025-06-06 10:58:29 +0200

    Update ROTEM_APP_FINAL.py

--- ROTEM_APP_FINAL.py
+++ ROTEM_APP_FINAL.py
@@ -167,7 +167,6 @@ if not st.session_state.show_advies:
             extem_ct, fibtem_a5, extem_a5, weight_kg, product_keuze
         )
         st.session_state.show_advies = True
-        st.experimental_rerun()
 
 # =======================
 # PAGINA 2 – Advies
@@ -181,7 +180,6 @@ else:
 
     if st.button("⬅️ Terug naar invoer"):
         st.session_state.show_advies = False
-        st.experimental_rerun()
 
 # =======================
 # Footer
