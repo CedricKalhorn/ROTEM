a001441f171584064c3ea9bdc0511bcddf861a0a
CedricKalhorn <c.a.f.kalhorn@student.tudelft.nl>
2025-06-06 11:40:52 +0200

    Update ROTEM_APP_FINAL.py

--- ROTEM_APP_FINAL.py
+++ ROTEM_APP_FINAL.py
@@ -151,7 +151,7 @@ with col_left:
 # =======================
 if not st.session_state.show_advies:
 
-    st.markdown("Geef hieronder de ROTEM-waarden en gewicht van de patiënt.")
+    st.markdown("Geef hieronder de gewicht van de patiënt en ROTEM-waarden.")
 
     col1, col2 = st.columns(2)
     with col1:
