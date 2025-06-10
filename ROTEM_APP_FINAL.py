2fec5f7ba3967dbd07e1a3efcd7679f9354d57e9
CedricKalhorn <c.a.f.kalhorn@student.tudelft.nl>
2025-06-10 15:45:46 +0200

    Update ROTEM_APP_FINAL.py

--- ROTEM_APP_FINAL.py
+++ ROTEM_APP_FINAL.py
@@ -197,6 +197,9 @@ else:
     for product, waarde in st.session_state.advies_resultaat.items():
         st.markdown(f"### {product}")
         st.markdown(f"<div class='advies-box'>{waarde}</div>", unsafe_allow_html=True)
+    if "Omniplasma" in st.session_state.advies_resultaat and "ml" in st.session_state.advies_resultaat["Omniplasma"]:
+        st.caption(f"ℹ️ Berekening gebaseerd op EXTEM CT = {extem_ct} sec en FIBTEM A5 = {fibtem_a5} mm.")
+
 
     st.caption("Dubbel klik indien nodig om terug te gaan naar invoerscherm")
     if st.button("⬅️ Terug naar invoerscherm"):
