b2d61742432d1b5f1d23c48dc464f4fc37046a57
CedricKalhorn <c.a.f.kalhorn@student.tudelft.nl>
2025-06-19 11:34:44 +0200

    Update ROTEM_APP_FINAL.py

--- ROTEM_APP_FINAL.py
+++ ROTEM_APP_FINAL.py
@@ -155,13 +155,13 @@ def stap_2_na_ROTEM_geleide_stollingscorrectie(extem_ct, fibtem_a5, extem_a5, ge
 
     advies = {}
     if keuze == "Cofact":
-           if totaal_flesjes > 0:
-                advies["Cofact"] = (
-                    f"{totaal_flesjes} flesje{'s' if totaal_flesjes>1 else ''} "
-                    f"({cofact_ml} ml = {cofact_ie} IE = {cofact_mg} mg) → "
-                    f"{cofact_flesjes_20ml}×20 ml, {cofact_flesjes_10ml}×10 ml"
-                )
-            else:
+        if totaal_flesjes > 0:
+            advies["Cofact"] = (
+                f"{totaal_flesjes} flesje{'s' if totaal_flesjes>1 else ''} "
+                f"({cofact_ml} ml = {cofact_ie} IE = {cofact_mg} mg) → "
+                f"{cofact_flesjes_20ml}×20 ml, {cofact_flesjes_10ml}×10 ml"
+            )
+        else:
                 advies["Cofact"] = "Geen toediening vereist"
     elif keuze == "Omniplasma":
         advies["Omniplasma"] = f"{omniplasma_zak} zakken ({omniplasma} ml)" if omniplasma_gebruikt else "Geen toediening vereist"
